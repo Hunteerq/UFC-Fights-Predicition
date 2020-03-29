dc08a24dc12aaa093ab253395dbb85c018bc3edf
piotr <p.kacprzak.96@gmail.com>
2020-03-29 03:33:03 +0200

    Fill missing categorical values with mode

--- lib/fill_missing_data.py
+++ lib/fill_missing_data.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 10000)
+
 
 class FillMissing:
     def __init__(self, pd_data):
@@ -8,15 +12,16 @@ class FillMissing:
     def get_values(self):
         return self.__data
 
-    def show(self):
-        pd.set_option('display.max_rows', 500)
-        pd.set_option('display.max_columns', 500)
-        pd.set_option('display.width', 10000)
-        print(self.__data)
+    def fill_missing_values(self):
+        self._fill_missing_numeric_values()
+        self._fill_missing_categorical_values()
 
-    def fill_missing_numeric_vals(self):
-        columns_array = self.__data.select_dtypes(include=['float64']).columns
+    def _fill_missing_numeric_values(self):
+        columns_array = self.__data.select_dtypes(include=['number']).columns
         self.__data[columns_array] = self.__data[columns_array].fillna(
             self.__data.groupby('weight_class')[columns_array].transform('mean'))
         self.__data[columns_array] = self.__data[columns_array].fillna(self.__data[columns_array].mean())
-        # print(self.data)
+
+    def _fill_missing_categorical_values(self):
+        columns_array = self.__data.select_dtypes(include=['object']).columns
+        self.__data[columns_array] = self.__data[columns_array].fillna(self.__data[columns_array].mode().iloc[0])
--- main.py
+++ main.py
@@ -4,5 +4,7 @@ from lib.fill_missing_data import FillMissing
 if __name__ == '__main__':
     de = DataExtractor()
     raw_data = de.get_raw_data()
-    filled_data = FillMissing(raw_data).get_values()
-    print(filled_data)
+    fm = FillMissing(raw_data)
+    fm.fill_missing_values()
+    filled_data = fm.get_values()
+    print(filled_data.isnull().any())
